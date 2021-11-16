dbbd4ce0946bde4cb4986afe6bea58316d6b9226
Christian Reza <creza@unomaha.edu>
2021-11-15 18:02:19 -0600

    messing with the file review form

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-review',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent implements OnInit {
+    
+  @Input()
+  public fileId: string | undefined;
+  
+  [x: string]: any;
+  baseUrl = "http://localhost:5000";
+  file: string = ''
+  display = false
+  slos:any[] = []
+  reports:any[] = []
+  academic_year: string = ''
+  accreditation_body: string = ''
+  additional_information: string = ''
+  author: string = ''
+  college: string = ''
+  created: string = ''
+  date_range: string = ''
+  degree_level: string = ''
+  department: string = ''
+  has_been_reviewed: string = ''
+  id: string = ''
+  last_accreditation_review: string = ''
+  program: string = ''
+  slos_meet_standards: string = ''
+  stakeholder_involvement: string = ''
+  title: string = ''
 
-  constructor() { }
+  constructor(/*private formBuilder: FormBuilder ,*/ private httpClient: HttpClient) { 
+    // this.reportForm = this.formBuilder.group({
+    //     academic_year: new FormControl('', [Validators.required, Validators.maxLength(32)])
+    // });
+  }
 
   ngOnInit(): void {
+    this.reportForm = this.formBuilder.group({
+        academic_year: new FormControl('', [Validators.required, Validators.maxLength(32)])
+    });
+  }
+
+  viewData(): any {
+    return this.httpClient.post(this.baseUrl + "/view/", this.file, {
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      }
+    });
   }
 
 }
