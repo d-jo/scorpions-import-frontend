26db37c92479d37cb2ee49caf2a2c132968c7728
Christian Reza <creza@unomaha.edu>
2021-11-17 20:30:27 -0600

    fix another mistype

--- src/app/report/ISlo.ts
+++ src/app/report/ISlo.ts
@@ -2,6 +2,6 @@ export interface ISlos {
     bloom: string[];
     common_graduate_program_slo: string[];
     description: string;
-    id: number;
-    report_id: string;
+    id: string;
+    report_id: number;
 }
