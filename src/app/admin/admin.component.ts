17b22c7a6166d8c955981e911606dcc32ad5d2fd
Christian Reza <creza@unomaha.edu>
2021-11-23 12:15:45 -0600

    remove unused import

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { timeStamp } from 'console';
 import { Observable } from 'rxjs';
 
 @Component({
